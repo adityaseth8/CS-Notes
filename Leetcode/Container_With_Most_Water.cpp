7f61612a12165ed8582a8d12b509b0423d48eced
pupawar <83674183+pupawar@users.noreply.github.com>
2023-07-01 12:37:47 -0700

    Update Container_With_Most_Water.cpp

--- Leetcode/Container_With_Most_Water.cpp
+++ Leetcode/Container_With_Most_Water.cpp
@@ -6,16 +6,19 @@ public:
         }
         return b;
     }
+    //O(n) time complexity. Space complexity don't know wtf that is.
     int maxArea(vector<int>& height) {
         int min = 0;
         int i  = 0;
         int j = height.size() -1;
+        // Create two pointers. Start and End. 
         while (i <= j){
             int h = min_height(height.at(i), height.at(j));
             int area = h*(j -i);
             if(area > min){
                 min = area;
             }
+            //Change the position of the pointer with the samller value.
             if(height.at(i) <= height.at(j)){
                 i++;
             }else {
